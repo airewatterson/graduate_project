6367ce053803583077fcb56fa7f0f6a7094547dc
airewatterson <airewatterson@gmail.com>
2021-09-23 00:55:12 +0800

    Transfer to new GitHub.

---
+++ Graduate_Project/Assets/BAG SYSTEM/tScript/slot.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slot : MonoBehaviour
+{
+
+    private bag bag;
+    private string Player = "Player";
+    public int elementCount;
+    private void Start()
+    {
+        bag = GameObject.FindGameObjectWithTag(Player).GetComponent<bag>();
+    }
+
+    private void Update()
+    {
+
+        if (transform.childCount <= 0) {
+            bag.isFull[elementCount] = false;
+        }
+    }
+
+    public void Dropitem()
+    {
+        foreach(Transform child in transform)
+        {
+            child.GetComponent<spawnDrop>().spawnDropitem();
+
+            GameObject.Destroy(child.gameObject);
+        }
+    }
+
+
+
+    
+}
---
+++ Graduate_Project/Assets/BAG SYSTEM/tScript/spawnDrop.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnDrop : MonoBehaviour
+{
+    public GameObject item;
+    private Transform tplayer;
+   
+
+
+    private string Player = "Player";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tplayer = GameObject.FindGameObjectWithTag(Player).transform;
+    }
+
+
+
+   public void spawnDropitem()
+    {
+        Vector2 playerPos = new Vector2(tplayer.position.x+ 2f, tplayer.position.y-0.5f);
+        Instantiate(item, playerPos, Quaternion.identity);
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
