6ad0697fbc1fc77f3f5aece287f20fb622088db4
airewatterson <airewatterson@gmail.com>
2021-09-27 15:21:56 +0800

    Dialog complete.

--- Graduate_Project/Assets/Scripts/Dialog.cs
+++ Graduate_Project/Assets/Scripts/Dialog.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using General;
 using UnityEngine;
@@ -7,19 +9,41 @@ using Image = UnityEngine.UI.Image;
 
 public class Dialog : SingletonMonoBehavior<Dialog>
 {
+    [FormerlySerializedAs("_textLabel")]
     [Header("UI Plugin")]
-    [SerializeField] private Text _textLabel;
-    [SerializeField] private Image _imageLabel;
+    [SerializeField] private Text textLabel;
+    [FormerlySerializedAs("_imageLabel")] [SerializeField] private Image imageLabel;
     
     [FormerlySerializedAs("_textAsset")]
     [Header("Plain Text Asset")]
     [SerializeField] private TextAsset textAsset;
-    [SerializeField] private int index;
+    [SerializeField] private  int index;
+    private bool _textFinished;
+    private bool _cancelTyping;
 
-    private readonly List<string> _text = new List<string>();
-    private void Start()
+    [FormerlySerializedAs("_av1")]
+    [Header("Avatar")] 
+    [SerializeField] private Sprite av1;
+    [FormerlySerializedAs("_av2")] [SerializeField] private Sprite av2;
+
+    [Header("Dialog Block")]
+    [SerializeField] private GameObject block;
+    
+    
+    [Header("Clock")]
+    [SerializeField] private float textClock;
+
+    [Header("Dialog Dictionary")] private  readonly List<string> _text = new List<string>();
+
+    public override void Awake()
     {
         GetText(textAsset);
+    }
+
+    private void OnEnable()
+    {
+        _textFinished = true;
+        StartCoroutine(nameof(SetText));
     }
 
     // Update is called once per frame
@@ -27,14 +51,29 @@ public class Dialog : SingletonMonoBehavior<Dialog>
     {
         if (Input.GetKeyDown(KeyCode.R) && index == _text.Count)
         {
-            gameObject.SetActive(false);
             index = 0;
+            block.SetActive(false);
             return;
         }
         
-        if (!Input.GetKeyDown(KeyCode.R)) return;
-        _textLabel.text = _text[index];
-        index++;
+        /*if (Input.GetKeyDown(KeyCode.R) && _textFinished)
+        {
+            StartCoroutine(nameof(SetText));
+        }*/
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (_textFinished && !_cancelTyping)
+            {
+                StartCoroutine(nameof(SetText));
+            }
+            else if (!_textFinished && !_cancelTyping)
+            {
+                _cancelTyping = true;
+            }
+        }
+        
+        //_textLabel.text = _text[index];
+        //index++;
     }
 
     private void GetText(TextAsset file)
@@ -49,4 +88,40 @@ public class Dialog : SingletonMonoBehavior<Dialog>
             _text.Add(line);
         }
     }
+    private IEnumerator SetText()
+    {
+        _textFinished = false;
+        textLabel.text = "";
+
+        switch (_text[index])
+        {
+            case "PLAYER":
+                imageLabel.sprite = av1;
+                index++; 
+                break;
+            case "NPC":
+                imageLabel.sprite = av2;
+                index++; 
+                break;
+        }
+
+        /*for (var i = 0; i < _text[index].Length; i++)
+        {
+            _textLabel.text += _text[index][i];
+            yield return new WaitForSeconds(textClock);
+        }*/
+        var letter = 0;
+        while (!_cancelTyping && letter < _text[index].Length - 1)
+        {
+            textLabel.text += _text[index][letter];
+            letter++;
+            yield return new WaitForSeconds(textClock);
+        }
+        textLabel.text = _text[index];
+        _cancelTyping = false;
+        _textFinished = true;
+        index++;
+    }
 }
+
+
