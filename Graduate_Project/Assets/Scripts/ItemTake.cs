b1cd893170ff9798cc98c2e1fa9d042dd6819a31
airewatterson <airewatterson@gmail.com>
2021-11-23 20:42:14 +0800

    Tag Update

---
+++ Graduate_Project/Assets/Scripts/ItemTake.cs
@@ -0,0 +1,16 @@
+using General;
+
+public class ItemTake : SingletonMonoBehavior<ItemTake>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
