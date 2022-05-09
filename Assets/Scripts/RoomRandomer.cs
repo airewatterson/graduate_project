00742159615428f6340f3ff5e4145928fb574954
airewatterson <airewatterson@gmail.com>
2022-05-10 03:59:12 +0800

    多人隨機地圖修正

--- Assets/Scripts/RoomRandomer.cs
+++ Assets/Scripts/RoomRandomer.cs
@@ -1,19 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
-using General;
+using Photon.Pun;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class RoomRandomer : SingletonMonoBehavior<RoomRandomer>
+public class RoomRandomer : MonoBehaviourPun
 {
+    [SerializeField] private int roomChoose;
+
+    [SerializeField] private GameObject scene1;
+    [SerializeField] private GameObject scene2;
+    [SerializeField] private GameObject scene3;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
+        roomChoose = Random.Range(1, 3);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Chooser();
+    }
+
+    private void Chooser()
+    {
+        switch (roomChoose)
+        {
+            case 1:
+                scene1.SetActive(true);
+                scene2.SetActive(false);
+                scene3.SetActive(false);
+                break;
+            case 2:
+                scene1.SetActive(false);
+                scene2.SetActive(true);
+                scene3.SetActive(false);
+                break;
+            case 3:
+                scene1.SetActive(false);
+                scene2.SetActive(false);
+                scene3.SetActive(true);
+                break;
+            default:
+                scene1.SetActive(false);
+                scene2.SetActive(false);
+                scene3.SetActive(false);
+                break;
+        }
     }
 }
